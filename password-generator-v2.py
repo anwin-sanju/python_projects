69b862f6c755954152e37d77dd2c9088aeda0bc7
anwin-sanju <dev.anwinsanju@gmail.com>
2024-05-09 20:05:08 +0530

    easy-gen

--- password-generator-v2.py
+++ password-generator-v2.py
@@ -7,3 +7,19 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+
+for char in range(1, nr_letters+1):
+    random_char = random.choice(letters)
+    password += random_char
+
+for num in range(1, nr_numbers+1):
+    random_num = random.choice(numbers)
+    password += random_num
+
+for sym in range(1,nr_symbols+1):
+    random_sym = random.choice(symbols)
+    password += random_sym
+
+print(password)
