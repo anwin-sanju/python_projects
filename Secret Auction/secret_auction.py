d2bc4e29c157084e2434315e2d412354373c0c3d
anwin-sanju <dev.anwinsanju@gmail.com>
2024-05-22 10:30:03 +0530

    secret-auction

---
+++ Secret Auction/secret_auction.py
@@ -0,0 +1,24 @@
+from replit import clear
+from art import logo
+print(logo)
+
+bids = {}
+
+finished = False
+
+while not finished:
+  name = input("What is your name? ")
+  bid = int(input("What is your bid? $"))
+  bids[name] = bid
+  more_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ")
+  if more_bidders == "no":
+      finished = True
+  clear()
+  for bidder in bids:
+    highest_bid = 0
+    winner = ""
+    bid_amount = bids[bidder]
+    if bid_amount > highest_bid:
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}.")
