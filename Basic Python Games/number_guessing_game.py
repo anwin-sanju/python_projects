f60b82e0ec80615dee5367c2a889918e36c517dd
anwin-sanju <dev.anwinsanju@gmail.com>
2024-05-23 23:02:12 +0530

    number-guessing-game

---
+++ Basic Python Games/number_guessing_game.py
@@ -0,0 +1,40 @@
+import random
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+def guess_number():
+  number = random.randint(1,100)
+  return number
+
+def compare(guess, number):
+  if guess == number:
+    return "You win!"
+  elif guess > number:
+    return "Too high."
+  else:
+    return "Too low."
+
+def play_game():
+  number = guess_number()
+  print(f"Pssst, the correct answer is {number}")
+  difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+  if difficulty == "easy":
+    attempts = 10
+  else:
+    attempts = 5
+    
+  while attempts > 0:
+    print(f"You have {attempts} attempts remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+    result = compare(guess, number)
+    if result == "You win!":
+      print(result)
+      return
+    else:
+      print(result)
+      attempts -= 1
+      if attempts < 1:
+        print("You've run out of guesses, you lose.")
+
+play_game()
