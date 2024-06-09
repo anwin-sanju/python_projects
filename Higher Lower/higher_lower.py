c124e2fe697440c7aa951312a1e358b4fc49bdb6
anwin-sanju <dev.anwinsanju@gmail.com>
2024-06-09 11:23:39 +0530

    check-answer

--- Higher Lower/higher_lower.py
+++ Higher Lower/higher_lower.py
@@ -7,6 +7,11 @@ def format_data(account):
     country = account["country"]
     return f"{name}, a {description}, from {country}"
 
+def check_answer(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
 
 from art import logo
 print(logo)
@@ -21,4 +26,8 @@ print(f"Compare A: {format_data(account_a)}")
 from art import vs
 print(vs)
 
-print(f"Compare B: {format_data(account_b)}")
+print(f"Against B: {format_data(account_b)}")
+
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
