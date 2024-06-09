3e98103cf0c7e2d5fe02486120586f9fa0aaa7f6
anwin-sanju <dev.anwinsanju@gmail.com>
2024-06-09 13:15:58 +0530

    process-coins

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -24,10 +24,42 @@ MENU = {
     }
 }
 
+def report():
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${profit}")
+
 profit = 0
+is_on = True
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients >= resources[item]:
+            print(f"Sorry, there is not enough {item}.")
+            return False
+    return True
+
+def process_coins():
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino):").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        report()
+    else:
+        drink = MENU[choice]
+        is_resource_sufficient(drink["ingredients"])
