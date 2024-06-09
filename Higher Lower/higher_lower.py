6ac0b22f978ece0b9f1170272897c310eff4832b
anwin-sanju <dev.anwinsanju@gmail.com>
2024-06-09 11:31:43 +0530

    repeatable

--- Higher Lower/higher_lower.py
+++ Higher Lower/higher_lower.py
@@ -13,21 +13,34 @@ def check_answer(guess, a_followers, b_followers):
     else:
         return guess == "b"
 
+
 from art import logo
 print(logo)
 
-account_a = random.choice(data)
-account_b = random.choice(data)
-if account_a == account_b:
+score = 0
+should_continue = True
+
+while should_continue:
+    account_a = random.choice(data)
     account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}")
+    print(f"Compare A: {format_data(account_a)}")
 
-from art import vs
-print(vs)
+    from art import vs
+    print(vs)
 
-print(f"Against B: {format_data(account_b)}")
+    print(f"Against B: {format_data(account_b)}")
 
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    if is_correct:
+        score +=1
+        print(f"You're right! Current score is {score}.")
+    else:
+        should_continue = False
+        print(f"Sorry, that's wrong! Final score is {score}.")
