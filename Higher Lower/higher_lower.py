56eed0107091e3c4ea11f0f101e5b977d2850385
anwin-sanju <dev.anwinsanju@gmail.com>
2024-06-09 11:11:49 +0530

    formating data from dictionary

--- Higher Lower/higher_lower.py
+++ Higher Lower/higher_lower.py
@@ -1,8 +1,24 @@
 import random
+from game_data import data
+
+def format_data(account):
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
+
 
 from art import logo
 print(logo)
 
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+    account_b = random.choice(data)
+
+print(f"Compare A: {format_data(account_a)}")
+
 from art import vs
 print(vs)
 
+print(f"Compare B: {format_data(account_b)}")
